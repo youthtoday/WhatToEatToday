faeea2cc30aa2191dac2f2ab37f2d7c9fd90808c
Lijie Huang <huanglj666@outlook.com>
2023-11-26 14:54:26 -0500

    gcp

--- src/main/java/com/whattoeattoday/recommendationservice/recommendation/VectorizedSimilarityServiceImpl.java
+++ src/main/java/com/whattoeattoday/recommendationservice/recommendation/VectorizedSimilarityServiceImpl.java
@@ -4,7 +4,9 @@ import com.whattoeattoday.recommendationservice.common.BaseResponse;
 import com.whattoeattoday.recommendationservice.common.Status;
 import com.whattoeattoday.recommendationservice.recommendation.request.GetVectorizedSimilarityRankOnMultiFieldRequest;
 import com.whattoeattoday.recommendationservice.recommendation.request.GetVectorizedSimilarityRankRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.Row;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 import com.google.api.gax.longrunning.OperationFuture;
@@ -28,13 +30,19 @@ import java.util.*;
  * @author Lijie Huang lh3158@columbia.edu
  * @date 10/19/23
  */
-@ConfigurationProperties(prefix = "spring")
+@Slf4j
+@ConfigurationProperties(prefix = "dataproc")
 @Service
 public class VectorizedSimilarityServiceImpl implements VectorizedSimilarityService{
 
-    private String projectId = "whattoeattoday-401920";
-    private String region = "us-central1";
-    private String clusterName = "cluster-9428";
+    @Value("${dataproc.project-id}")
+    private String projectId;
+
+    @Value("${dataproc.region}")
+    private String region;
+
+    @Value("${dataproc.cluster-name}")
+    private String clusterName;
 
     @Override
     public BaseResponse<List<Row>> getVectorizedSimilarityRank(GetVectorizedSimilarityRankRequest request) {
@@ -46,6 +54,9 @@ public class VectorizedSimilarityServiceImpl implements VectorizedSimilarityServ
     public BaseResponse<List<String>> getVectorizedSimilarityRankOnMultiField(GetVectorizedSimilarityRankOnMultiFieldRequest request) throws IOException, ExecutionException, InterruptedException {
         // TODO Param Check
         // TODO Double Type Unsupported
+        String mainClass = "com.whattoeattoday.RecommendOnItem";
+        String jarFileUris = "gs://4156-recommend-job/Recommend-1.0-SNAPSHOT.jar";
+
         String tableName = request.getCategoryName();
         List<String> fieldNameArr = request.getFieldNameList();
         StringBuilder fieldNameSb = new StringBuilder();
@@ -72,13 +83,17 @@ public class VectorizedSimilarityServiceImpl implements VectorizedSimilarityServ
             // Configure cluster placement for the job.
             JobPlacement jobPlacement = JobPlacement.newBuilder().setClusterName(clusterName).build();
 
-            String[] argsArr = new String[]{tableName, fieldNameSb.toString(), request.getTargetId(), String.valueOf(request.getRankTopSize()), "result"};
+            String[] argsArr = new String[]{
+                    tableName,
+                    fieldNameSb.toString(),
+                    request.getTargetId(),
+                    String.valueOf(request.getRankTopSize())};
             List<String> args = Arrays.asList(argsArr);
             // Configure Spark job settings.
             SparkJob sparkJob =
                     SparkJob.newBuilder()
-                            .setMainClass("com.whattoeattoday.RecommendOnItem")
-                            .addJarFileUris("gs://4156-recommend-job/Recommend-1.0-SNAPSHOT.jar")
+                            .setMainClass(mainClass)
+                            .addJarFileUris(jarFileUris)
                             .addAllArgs(args)
                             .build();
 
@@ -90,8 +105,6 @@ public class VectorizedSimilarityServiceImpl implements VectorizedSimilarityServ
 
             Job response = submitJobAsOperationAsyncRequest.get();
 
-            System.out.println("response.getDriverOutputResourceUri(): "+ response.getDriverOutputResourceUri());
-
             // Print output from Google Cloud Storage.
             Matcher matches =
                     Pattern.compile("gs://(.*?)/(.*)").matcher(response.getDriverOutputResourceUri());
@@ -101,17 +114,15 @@ public class VectorizedSimilarityServiceImpl implements VectorizedSimilarityServ
             Blob blob = storage.get(matches.group(1), String.format("%s.000000000", matches.group(2)));
 
             String dataprocLog = new String(blob.getContent());
-            System.out.println(
-                    String.format("Job finished successfully: %s", dataprocLog));
+            log.info("Dataproc job finished successfully: {}", dataprocLog);
 
             String[] dataprocLogLines = dataprocLog.split("\\r?\\n");
-            System.out.printf("Last line: %s%n", dataprocLogLines[dataprocLogLines.length-1]);
-
             String resultStr = dataprocLogLines[dataprocLogLines.length-1];
             String[] resultArr = resultStr.split(",");
             resultList = Arrays.asList(resultArr);
             return BaseResponse.with(Status.SUCCESS, resultList);
         } catch (Exception ignored) {}
+
         return BaseResponse.with(Status.FAILURE, "Dataproc Error");
     }
 }
